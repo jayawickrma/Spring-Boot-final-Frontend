9d1ccb71e99babdb7c7bd79799aa7422cea57708
jayawickrama <nishantharaka2004@gmail.com>
2024-11-03 21:13:36 +0530

    creating js

--- Model/fieldModel.js
+++ Model/fieldModel.js
@@ -11,4 +11,67 @@ export default class field{
     }
 
 
+    get code() {
+        return this._code;
+    }
+
+    set code(value) {
+        this._code = value;
+    }
+
+    get name() {
+        return this._name;
+    }
+
+    set name(value) {
+        this._name = value;
+    }
+
+    get location() {
+        return this._location;
+    }
+
+    set location(value) {
+        this._location = value;
+    }
+
+    get size() {
+        return this._size;
+    }
+
+    set size(value) {
+        this._size = value;
+    }
+
+    get cropId() {
+        return this._cropId;
+    }
+
+    set cropId(value) {
+        this._cropId = value;
+    }
+
+    get staffID() {
+        return this._staffID;
+    }
+
+    set staffID(value) {
+        this._staffID = value;
+    }
+
+    get img1() {
+        return this._img1;
+    }
+
+    set img1(value) {
+        this._img1 = value;
+    }
+
+    get img2() {
+        return this._img2;
+    }
+
+    set img2(value) {
+        this._img2 = value;
+    }
 }
