d2b2ac600f75335d34ddaf83f4cf294f53d88bff
jayawickrama <nishantharaka2004@gmail.com>
2024-11-03 20:51:33 +0530

    creating js

---
+++ Model/fieldModel.js
@@ -0,0 +1,12 @@
+export default class field{
+    constructor(code,name,location,size,cropId,staffID,img1,img2) {
+                this._code=code;
+                this._name=name;
+                this._location=location;
+                this._size=size;
+                this._cropId=cropId;
+                this._staffID=staffID;
+                this._img1=img1;
+                this._img2=img2;
+    }
+}
