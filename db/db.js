da4c6ca01d04f29303621d453ff70e0a92f387be
jayawickrama <nishantharaka2004@gmail.com>
2024-11-04 21:32:19 +0530

    edited

--- db/db.js
+++ db/db.js
@@ -1,6 +1,6 @@
 export let fieldDetails =[];
 export let cropDetails=[];
-export let staffDetails=[];
+export let staffData=[];
 export let logsDetails=[];
 export let vehicleDetails=[];
 export let equipmentDetails=[];
