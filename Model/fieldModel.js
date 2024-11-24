2b7cbcb3f5e9d89070426c37a1ce2983fc9b7245
jayawickrama <nishantharaka2004@gmail.com>
2024-11-24 21:26:32 +0530

    edited

--- Model/fieldModel.js
+++ Model/fieldModel.js
@@ -1,77 +1,92 @@
-export default class field{
-    constructor(code,name,location,size,cropId,staffID,img1,img2) {
-                this._code=code;
-                this._name=name;
-                this._location=location;
-                this._size=size;
-                this._cropId=cropId;
-                this._staffID=staffID;
-                this._img1=img1;
-                this._img2=img2;
+export default class Field {
+    constructor(code, name, location, size, cropId, staffID, img1, img2) {
+        this._code = code;
+        this._name = name;
+        this._location = location;
+        this._size = size;
+        this._cropId = cropId;
+        this._staffID = staffID;
+        this._img1 = img1;
+        this._img2 = img2;
     }
 
-
+    // Getters
     get code() {
         return this._code;
-    }
-
-    set code(value) {
-        this._code = value;
     }
 
     get name() {
         return this._name;
     }
 
-    set name(value) {
-        this._name = value;
-    }
-
     get location() {
         return this._location;
-    }
-
-    set location(value) {
-        this._location = value;
     }
 
     get size() {
         return this._size;
     }
 
-    set size(value) {
-        this._size = value;
-    }
-
     get cropId() {
         return this._cropId;
-    }
-
-    set cropId(value) {
-        this._cropId = value;
     }
 
     get staffID() {
         return this._staffID;
     }
 
-    set staffID(value) {
-        this._staffID = value;
-    }
-
     get img1() {
         return this._img1;
-    }
-
-    set img1(value) {
-        this._img1 = value;
     }
 
     get img2() {
         return this._img2;
     }
 
+    // Setters
+    set code(value) {
+        this._code = value;
+    }
+
+    set name(value) {
+        this._name = value;
+    }
+
+    set location(value) {
+        this._location = value;
+    }
+
+    set size(value) {
+        this._size = value;
+    }
+
+    set cropId(value) {
+        this._cropId = value;
+    }
+
+    set staffID(value) {
+        this._staffID = value;
+    }
+
+    set img1(value) {
+        this._img1 = value;
+    }
+
     set img2(value) {
         this._img2 = value;
+    }
+
+    // toString method
+    toString() {
+        return `Field {
+            Code: ${this._code},
+            Name: ${this._name},
+            Location: ${this._location},
+            Size: ${this._size},
+            Crop ID: ${this._cropId},
+            Staff ID: ${this._staffID},
+            Image 1: ${this._img1},
+            Image 2: ${this._img2}
+        }`;
     }
 }
--- controller/cropController.js
+++ controller/cropController.js
@@ -1,74 +1,73 @@
-
-
-$(document).ready(function() {
+$(document).ready(function () {
     var crops = [];
     var editIndex = -1;
 
     // Save or Update Crop
-    $('#cropForm').submit(function(e) {
+    $('#cropForm').submit(function (e) {
         e.preventDefault();
+
         var cropName = $('#cropName').val();
         var season = $('#Season').val();
         var scientificName = $('#ScientificName').val();
-        var image = $('#Image').val();
-        var field = $('#Field').val();
+        var imageFile = $('#CropImage')[0].files[0]; // Get file object
+        var field = $('#field').val();
 
-        if (editIndex === -1) {
-            var newCrop = {
-                id: crops.length + 1,
-                name: cropName,
-                season: season,
-                scientificName: scientificName,
-                image: image,
-                field:field
+        if (imageFile) {
+            var reader = new FileReader();
+            reader.onload = function (event) {
+                var imageUrl = event.target.result;
+
+                if (editIndex === -1) {
+                    // Add new crop
+                    crops.push({
+                        id: crops.length + 1,
+                        name: cropName,
+                        season: season,
+                        scientificName: scientificName,
+                        image: imageUrl,
+                        field: field,
+                    });
+                } else {
+                    // Update crop
+                    crops[editIndex].name = cropName;
+                    crops[editIndex].season = season;
+                    crops[editIndex].scientificName = scientificName;
+                    crops[editIndex].image = imageUrl;
+                    crops[editIndex].field = field;
+                    editIndex = -1;
+                }
+
+                $('#cropModal').modal('hide'); // Close modal
+                $('#cropForm')[0].reset(); // Reset form
+                renderTable(); // Refresh table
             };
-            crops.push(newCrop);
-        } else {
+            reader.readAsDataURL(imageFile); // Convert image to Base64
+        } else if (editIndex !== -1) {
             crops[editIndex].name = cropName;
             crops[editIndex].season = season;
             crops[editIndex].scientificName = scientificName;
-            crops[editIndex].image=image
-            crop[editIndex].field = field
-            editIndex = -1;
+            crops[editIndex].field = field;
+            $('#cropModal').modal('hide');
+            renderTable();
         }
-
-        $('#cropModal').modal('hide');
-        $('#cropForm')[0].reset();
-        renderTable();
-    });
-
-    // Edit Crop
-    $(document).on('click', '.edit-btn', function() {
-        var id = $(this).data('id');
-        editIndex = crops.findIndex(crop => crop.id === id);
-        var crop = crops[editIndex];
-        $('#cropName').val(crop.name);
-        $('#Season').val(crop.season);
-        $('#ScientificName').val(crop.scientificName);
-        $('#CropImage').val(crop.image);
-        $('#Field').val(crop.field);
-        $('#cropModalLabel').text('Edit Crop');
-        $('#cropModal').modal('show');
-    });
-
-    // Delete Crop
-    $(document).on('click', '.delete-btn', function() {
-        var id = $(this).data('id');
-        crops = crops.filter(crop => crop.id !== id);
-        renderTable();
     });
 
     // Render Table
     function renderTable() {
-        $('#cropTable tbody').empty();
-        crops.forEach(crop => {
-            $('#cropTable tbody').append(`
+        var tbody = $('#cropTable tbody');
+        tbody.empty(); // Clear the table body
+
+        crops.forEach(function (crop) {
+            tbody.append(`
                 <tr>
                     <td>${crop.id}</td>
                     <td>${crop.name}</td>
                     <td>${crop.season}</td>
                     <td>${crop.scientificName}</td>
-                    <td>${crop.image}</td>
+                    <td>
+                        <img src="${crop.image}" alt="${crop.name}" class="crop-img" data-image="${crop.image}" style="width: 50px; height: 50px; cursor: pointer;">
+                    </td>
+                    <td>${crop.field}</td>
                     <td>
                         <button class="btn btn-primary edit-btn" data-id="${crop.id}">Edit</button>
                         <button class="btn btn-danger delete-btn" data-id="${crop.id}">Delete</button>
@@ -77,4 +76,44 @@ $(document).ready(function() {
             `);
         });
     }
+
+    // Show Image in Modal
+    $(document).on('click', '.crop-img', function () {
+        var imageUrl = $(this).data('image');
+        $('#imageModal .modal-body img').attr('src', imageUrl); // Set modal image source
+        $('#imageModal').modal('show'); // Show the modal
+    });
+
+    // Edit Crop
+    $(document).on('click', '.edit-btn', function () {
+        var id = $(this).data('id');
+        editIndex = crops.findIndex(function (crop) {
+            return crop.id === id;
+        });
+
+        var crop = crops[editIndex];
+        $('#cropName').val(crop.name);
+        $('#Season').val(crop.season);
+        $('#ScientificName').val(crop.scientificName);
+        $('#field').val(crop.field);
+
+        $('#cropModalLabel').text('Edit Crop');
+        $('#cropModal').modal('show');
+    });
+
+    // Delete Crop
+    $(document).on('click', '.delete-btn', function () {
+        var id = $(this).data('id');
+        crops = crops.filter(function (crop) {
+            return crop.id !== id;
+        });
+        renderTable();
+    });
+
+    // Open Add Crop Modal
+    $('#cropSaveBtn').click(function () {
+        editIndex = -1; // Reset edit index
+        $('#cropForm')[0].reset(); // Clear form
+        $('#cropModalLabel').text('Add Crop');
+    });
 });
