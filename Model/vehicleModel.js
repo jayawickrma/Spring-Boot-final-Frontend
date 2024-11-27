403ca0dea5cf1fe8eba97127461615f6fbb97743
jayawickrama <nishantharaka2004@gmail.com>
2024-11-27 22:39:15 +0530

    edited

--- Model/vehicleModel.js
+++ Model/vehicleModel.js
@@ -1,76 +1,76 @@
-export default class {
-    get vehicleCode() {
-        return this._vehicleCode;
-    }
+    export default class {
+        get vehicleCode() {
+            return this._vehicleCode;
+        }
 
-    set vehicleCode(value) {
-        this._vehicleCode = value;
-    }
+        set vehicleCode(value) {
+            this._vehicleCode = value;
+        }
 
-    get licencePlateNumber() {
-        return this._licencePlateNumber;
-    }
+        get licencePlateNumber() {
+            return this._licencePlateNumber;
+        }
 
-    set licencePlateNumber(value) {
-        this._licencePlateNumber = value;
-    }
+        set licencePlateNumber(value) {
+            this._licencePlateNumber = value;
+        }
 
-    get name() {
-        return this._name;
-    }
+        get name() {
+            return this._name;
+        }
 
-    set name(value) {
-        this._name = value;
-    }
+        set name(value) {
+            this._name = value;
+        }
 
-    get category() {
-        return this._category;
-    }
+        get category() {
+            return this._category;
+        }
 
-    set category(value) {
-        this._category = value;
-    }
+        set category(value) {
+            this._category = value;
+        }
 
-    get fuelType() {
-        return this._fuelType;
-    }
+        get fuelType() {
+            return this._fuelType;
+        }
 
-    set fuelType(value) {
-        this._fuelType = value;
-    }
+        set fuelType(value) {
+            this._fuelType = value;
+        }
 
-    get remark() {
-        return this._remark;
-    }
+        get remark() {
+            return this._remark;
+        }
 
-    set remark(value) {
-        this._remark = value;
-    }
+        set remark(value) {
+            this._remark = value;
+        }
 
-    get status() {
-        return this._status;
-    }
+        get status() {
+            return this._status;
+        }
 
-    set status(value) {
-        this._status = value;
-    }
+        set status(value) {
+            this._status = value;
+        }
 
-    get memberCode() {
-        return this._memberCode;
-    }
+        get memberCode() {
+            return this._memberCode;
+        }
 
-    set memberCode(value) {
-        this._memberCode = value;
-    }
-    constructor(vehicleCode,licencePlateNumber,name,category,fuelType,remark,status,memberCode) {
+        set memberCode(value) {
+            this._memberCode = value;
+        }
+        constructor(vehicleCode,licencePlateNumber,name,category,fuelType,remark,status,memberCode) {
 
-        this._vehicleCode = vehicleCode;
-        this._licencePlateNumber = licencePlateNumber;
-        this._name = name;
-        this._category = category;
-        this._fuelType = fuelType;
-        this._remark = remark;
-        this._status = status;
-        this._memberCode = memberCode;
+            this._vehicleCode = vehicleCode;
+            this._licencePlateNumber = licencePlateNumber;
+            this._name = name;
+            this._category = category;
+            this._fuelType = fuelType;
+            this._remark = remark;
+            this._status = status;
+            this._memberCode = memberCode;
+        }
     }
-}
--- controller/vehicleControlller.js
+++ controller/vehicleControlller.js
@@ -1,92 +1,120 @@
-import {vehicles} from "../db/db.js";
 $(document).ready(function () {
     var editIndex = -1;
+
+    // Fetch and load data from the backend
+    function loadVehicles() {
+        $.ajax({
+            url: "http://localhost:8080/springFinal/api/v1/lvehicles",
+            method: "GET",
+            success: function (data) {
+                renderVehicleTable(data);
+            },
+            error: function () {
+                alert("Failed to load vehicles!");
+            }
+        });
+    }
+
+    // Render Vehicle Table
+    function renderVehicleTable(vehicles) {
+        $('#vehicleTable tbody').empty();
+        vehicles.forEach((vehicle, index) => {
+            $('#vehicleTable tbody').append(`
+                <tr>
+                    <td>${vehicle.vehicleCode}</td>
+                    <td>${vehicle.licensePlateNumber}</td>
+                    <td>${vehicle.name}</td>
+                    <td>${vehicle.category}</td>
+                    <td>${vehicle.fuelType}</td>
+                    <td>${vehicle.remark}</td>
+                    <td>${vehicle.status}</td>
+                    <td>${vehicle.memberCode}</td>
+                    <td>
+                        <button class="btn btn-primary edit-vehicle-btn" data-id="${vehicle.vehicleCode}" data-index="${index}">Edit</button>
+                        <button class="btn btn-danger delete-vehicle-btn" data-id="${vehicle.vehicleCode}">Delete</button>
+                    </td>
+                </tr>
+            `);
+        });
+    }
 
     // Save or Update Vehicle
     $('#vehicleForm').submit(function (e) {
         e.preventDefault();
 
-        var licensePlate = $('#licensePlate').val();
-        var name = $('#vehicleName').val();
-        var category = $('#vehicleCategory').val();
-        var fuelType = $('#fuelType').val();
-        var remark = $('#remark').val();
-        var status = $('#status').val();
-        var staffId = $('#staffId').val(); // Get Staff ID
+        const vehicle = {
+            licensePlateNumber: $('#licensePlate').val(),
+            name: $('#vehicleName').val(),
+            category: $('#vehicleCategory').val(),
+            fuelType: $('#fuelType').val(),
+            remark: $('#remark').val(),
+            status: $('#status').val(),
+            memberCode: $('#staffId').val()
+        };
 
-        if (editIndex === -1) {
-            // Add new vehicle
-            var newVehicle = {
-                id: vehicles.length + 1,
-                licensePlate: licensePlate,
-                name: name,
-                category: category,
-                fuelType: fuelType,
-                remark: remark,
-                status: status,
-                staffId: staffId, // Store Staff ID
-            };
-            vehicles.push(newVehicle);
-        } else {
-            // Update existing vehicle
-            vehicles[editIndex].licensePlate = licensePlate;
-            vehicles[editIndex].name = name;
-            vehicles[editIndex].category = category;
-            vehicles[editIndex].fuelType = fuelType;
-            vehicles[editIndex].remark = remark;
-            vehicles[editIndex].status = status;
-            vehicles[editIndex].staffId = staffId; // Update Staff ID
-            editIndex = -1;
-        }
+        const url = editIndex === -1
+            ? "http://localhost:8080/springFinal/api/v1/lvehicles"
+            : `http://localhost:8080/springFinal/api/v1/lvehicles/${editIndex}`;
+        const method = editIndex === -1 ? "POST" : "PUT";
 
-        $('#vehicleModal').modal('hide');
-        $('#vehicleForm')[0].reset();
-        renderVehicleTable();
-    });
-
-    // Render Vehicle Table
-    function renderVehicleTable() {
-        $('#vehicleTable tbody').empty();
-        vehicles.forEach((vehicle, index) => {
-            $('#vehicleTable tbody').append(`
-        <tr>
-          <td>${vehicle.licensePlate}</td>
-          <td>${vehicle.name}</td>
-          <td>${vehicle.category}</td>
-          <td>${vehicle.fuelType}</td>
-          <td>${vehicle.remark}</td>
-          <td>${vehicle.status}</td>
-          <td>${vehicle.staffId}</td> <!-- Display Staff ID -->
-          <td>
-            <button class="btn btn-primary edit-vehicle-btn" data-index="${index}">Edit</button>
-            <button class="btn btn-danger delete-vehicle-btn" data-index="${index}">Delete</button>
-          </td>
-        </tr>
-      `);
+        $.ajax({
+            url: url,
+            method: method,
+            contentType: "application/json",
+            data: JSON.stringify(vehicle),
+            success: function () {
+                $('#vehicleModal').modal('hide');
+                $('#vehicleForm')[0].reset();
+                loadVehicles();
+            },
+            error: function () {
+                alert("Failed to save vehicle!");
+            }
         });
-    }
+    });
 
     // Edit Vehicle
     $(document).on('click', '.edit-vehicle-btn', function () {
-        editIndex = $(this).data('index');
-        var vehicle = vehicles[editIndex];
+        const id = $(this).data("id");
+        editIndex = id;
 
-        $('#licensePlate').val(vehicle.licensePlate);
-        $('#vehicleName').val(vehicle.name);
-        $('#vehicleCategory').val(vehicle.category);
-        $('#fuelType').val(vehicle.fuelType);
-        $('#remark').val(vehicle.remark);
-        $('#status').val(vehicle.status);
-        $('#staffId').val(vehicle.staffId); // Set Staff ID in the form
+        $.ajax({
+            url: `http://localhost:8080/springFinal/api/v1/lvehicles/${id}`,
+            method: "GET",
+            success: function (vehicle) {
+                $('#licensePlate').val(vehicle.licensePlateNumber);
+                $('#vehicleName').val(vehicle.name);
+                $('#vehicleCategory').val(vehicle.category);
+                $('#fuelType').val(vehicle.fuelType);
+                $('#remark').val(vehicle.remark);
+                $('#status').val(vehicle.status);
+                $('#staffId').val(vehicle.memberCode);
 
-        $('#vehicleModalLabel').text('Edit Vehicle');
-        $('#vehicleModal').modal('show');
+                $('#vehicleModalLabel').text('Edit Vehicle');
+                $('#vehicleModal').modal('show');
+            },
+            error: function () {
+                alert("Failed to fetch vehicle data!");
+            }
+        });
     });
 
     // Delete Vehicle
     $(document).on('click', '.delete-vehicle-btn', function () {
-        var index = $(this).data('index');
-        vehicles.splice(index, 1);
-        renderVehicleTable();
+        const id = $(this).data("id");
+
+        $.ajax({
+            url: `http://localhost:8080/springFinal/api/v1/lvehicles/${id}`,
+            method: "DELETE",
+            success: function () {
+                loadVehicles();
+            },
+            error: function () {
+                alert("Failed to delete vehicle!");
+            }
+        });
     });
+
+    // Initial Load
+    loadVehicles();
 });
